f2b81cd52423a4324fd9d0ac9451ff2bbc4f1916
IBAD ULLAH KHAN <ibad50008@gmail.com>
2025-07-21 08:53:06 +0500

    started the react
    day one learned how to create react app and edit the main the page in the react app

---
+++ 01project/src/App.js
@@ -0,0 +1,22 @@
+
+
+function App() {
+  return (
+    <div>
+    <h1>chia Aur code  React | Ibad ullah Khan "</h1>
+    <h2>What I learned Today </h2>
+    <ol>
+      <li><h3>How to create a react app</h3><ul><li>write a command in the VS terminal npx create-react-app <b>project01</b> and your project will be created <br/>
+       the bold key word is teh project name what ever you like to write here you can </li></ul></li>
+       <li><h3>How to run ? </h3><ul><li>write a command in the terminal <br/> npm run start <br/> npm run build - which will aslo create a folder build the project file </li></ul></li>
+    </ol>
+    <ol>
+      <li><h3>How to create a react app through vite</h3><ul><li>write a command in the VS terminal npm create vite@latest and your project will be created <br/>
+       then add some more detials after command like porject name etc </li></ul></li>
+       <li><h3>How to run ? </h3><ul><li>write a command in the terminal <br/> npm run dev </li></ul></li>
+    </ol>
+    </div>
+  );
+}
+
+export default App;
