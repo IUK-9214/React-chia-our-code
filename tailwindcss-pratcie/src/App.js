25a055fd4a53c1d272447cc05cea3e401bf34613
IBAD ULLAH KHAN <ibad50008@gmail.com>
2025-07-30 12:05:45 -0700

    tailwind css installed
    finally i  installed the tailwind css in my project  the version is 3.4.17

---
+++ tailwindcss-pratcie/src/App.js
@@ -0,0 +1,14 @@
+
+import './index.css'
+
+function App() {
+  return (
+    <>
+    <div>
+      <h1 className='bg-black text-white'>created </h1>
+    </div>
+    </>
+  );
+}
+
+export default App;
