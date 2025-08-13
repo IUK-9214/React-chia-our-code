4b3d52cd91b212bdef7c2dade68e823ece70c728
IBAD ULLAH KHAN <ibad50008@gmail.com>
2025-08-13 02:42:09 -0700

    update auth.js
    create a function for the login and creating account

--- Mega-Project/src/appwrite/auth.js
+++ Mega-Project/src/appwrite/auth.js
@@ -1,17 +1,66 @@
 import config from "../config";
-import { Client,Account,ID } from "appwrite";
+import { Client, Account, ID } from "appwrite";
 
-export class AuthServices{
-client=new Client;
-account;
+export class AuthServices {
+    client = new Client;
+    account;
 
-constructor (){
-    this.client
-    .setEndpoint(config.AppwriteUrl)
-    .setProject(config.AppwriteProjectId)
-    this.account=new Account(this.client)
-        
-}
+    constructor() {
+        this.client
+            .setEndpoint(config.AppwriteUrl)
+            .setProject(config.AppwriteProjectId)
+        this.account = new Account(this.client)
+
+    }
+    //create an account service
+    async createAccount({ email, password, name }) {
+
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name)
+            if (userAccount) {
+                // direct him to the login page 
+                this.Login({ email, password })
+            }
+            else {
+                return userAccount;
+            }
+
+        } catch (error) {
+            throw error;
+        }
+    }
+    // create a login services 
+    async Login({ email, password }) {
+
+        try {
+
+            return await this.account.createEmailPasswordSession(email, password)
+
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    //get alluser services
+    async getCurrentUser() {
+        try {
+            return await this.account.get()
+
+        } catch (error) {
+            console.log(" Appwrite service :: getCurrentUser : :error ", error)
+        }
+        return null;
+    }
+
+    //logout all the user 
+    async Logout() {
+        try {
+            return await this.account.deleteSessions()
+        } catch (error) {
+            console.log(" Appwrite service :: logout :: error  ", error)
+        }
+
+    }
 
 
 }
